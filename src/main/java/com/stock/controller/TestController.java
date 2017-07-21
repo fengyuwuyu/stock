c55da7ddc5a43d46ec0c10f881378a0f4a2954cb
lilei <lilei@vwvo.net>
2017-07-21 20:09:42 +0800

    xx

--- src/main/java/com/stock/controller/TestController.java
+++ src/main/java/com/stock/controller/TestController.java
@@ -10,8 +10,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.stock.dao.StockBuySellMapper;
 import com.stock.dao.StockMainMapper;
 import com.stock.model.StockBuySell;
+import com.stock.model.StockBuySellGroup;
 import com.stock.service.InitStockServiceI;
 import com.stock.service.SearchMachineI;
 import com.stock.service.StockAnalyseJobI;
@@ -29,6 +31,8 @@ public class TestController {
 	private InitStockServiceI initStockServiceI;
 	private StockAnalyseJobI stockAnalyseJobI;
 	private StockMainMapper stockMainMapper;
+	@Autowired
+	private StockBuySellMapper buySellMapper;
 	private SearchMachineI searchMachineI;
 	private Logger log = Logger.getLogger(TestController.class);
 	
@@ -65,64 +69,21 @@ public class TestController {
 	@RequestMapping("test.do")
 	@ResponseBody
 	public Map<String, Object> test() {
-		initStockServiceI.initBuyAndSell("2017-07-21");
+//		initStockServiceI.initBuyAndSell("2017-07-21");
+		List<StockBuySellGroup> buySellGroups = buySellMapper.selectRecord();
+		if(buySellGroups != null && buySellGroups.size() > 0){
+			for (StockBuySellGroup stockBuySellGroup : buySellGroups) {
+				System.out.println(stockBuySellGroup);
+			}
+		}
 		return MapUtils.createFailedMap();
 	}
 
 	@RequestMapping("test1.do")
 	@ResponseBody
 	public Map<String, Object> test1() throws Exception {
-//		initStockServiceI.initCjmxPerWeek();
-
-		List<StockBuySell> list = new ArrayList<StockBuySell>();
-		String day = "2017-07-21";
-		String code = "000001";
-		String symbol = RandomUtil.randomString(6);
-		double percent = RandomUtil.randomFloat(0, 10);
-		double high = RandomUtil.randomFloat(0, 10);
-		double price = RandomUtil.randomFloat(0, 10);
-		double open = RandomUtil.randomFloat(0, 10);
-		double low = RandomUtil.randomFloat(0, 10);
-		String time = RandomUtil.randomString(10);
-		double yestclose = RandomUtil.randomFloat(0, 10);
-		long turnover = RandomUtil.randomLong();
-		double updown = RandomUtil.randomFloat(0, 10);
-		int volume = RandomUtil.randomInt(0, 10);
-		double ask1 = RandomUtil.randomFloat(0, 10);
-		double ask2 = RandomUtil.randomFloat(0, 10);
-		double ask3 = RandomUtil.randomFloat(0, 10);
-		double ask4 = RandomUtil.randomFloat(0, 10);
-		double ask5 = RandomUtil.randomFloat(0, 10);
-		int askvol1 = RandomUtil.randomInt(0, 12);
-		int askvol2 = RandomUtil.randomInt(0, 12);
-		int askvol3 = RandomUtil.randomInt(0, 12);
-		int askvol4 = RandomUtil.randomInt(0, 12);
-		int askvol5 = RandomUtil.randomInt(0, 12);
-		double bid1 = RandomUtil.randomFloat(0, 10);
-		double bid2 = RandomUtil.randomFloat(0, 10);
-		double bid3 = RandomUtil.randomFloat(0, 10);
-		double bid4 = RandomUtil.randomFloat(0, 10);
-		double bid5 = RandomUtil.randomFloat(0, 10);
-		int bidvol1 = RandomUtil.randomInt(0, 12);
-		int bidvol2 = RandomUtil.randomInt(0, 12);
-		int bidvol3 = RandomUtil.randomInt(0, 12);
-		int bidvol4 = RandomUtil.randomInt(0, 12);
-		int bidvol5 = RandomUtil.randomInt(0, 12);
-		StockBuySell buySell = new StockBuySell(code, symbol, percent, high, price, open, low, time, yestclose, turnover, updown, volume, ask1, ask2, ask3, 
-				ask4, ask5, askvol1, askvol2, askvol3, askvol4, askvol5, bid1, bid2, bid3, bid4, bid5, bidvol1, bidvol2, bidvol3, bidvol4, bidvol5, day);
-		list.add(buySell);
-		stockMainMapper.insertStockBuySell(MapUtils.createMap("list", list,"day",day));
-//		stockAnalyseJobI.initStockAnalyse();
-//		List<String> symbols = stockMainMapper.selectAll();
-//		int index = 0;
-//		if(symbols!=null&&symbols.size()>0){
-//			for (String symbol : symbols) {
-//				initStockServiceI.initJunX(symbol);
-//				log.info("--------------------------"+(++index));
-//			}
-//		}
+		
 		return MapUtils.createSuccessMap();
-		// this.detailSaveServiceI.volBigIncrease();
 	}
 	
 	public static void main(String[] args) {
--- src/main/java/com/stock/service/impl/StatisticsStockImpl.java
+++ src/main/java/com/stock/service/impl/StatisticsStockImpl.java
@@ -1,5 +1,6 @@
 package com.stock.service.impl;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,22 +8,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.stock.dao.LastStatisticsDayMapper;
 import com.stock.dao.StockBuySellMapper;
+import com.stock.dao.StockMainMapper;
 import com.stock.model.StockBuySell;
 import com.stock.service.StatisticsStockI;
+import com.stock.util.MapUtils;
 
 public class StatisticsStockImpl implements StatisticsStockI {
+	Map<String, Map<String, Object>> cache = new HashMap<String, Map<String, Object>>();
 
 	@Autowired
 	private LastStatisticsDayMapper lastStatisticsDayMapper;
 	@Autowired
+	private StockMainMapper mainMapper;
+	@Autowired
 	private StockBuySellMapper stockBuySellMapper;
 
 	public Map<String, Object> statistics() {
+		List<String> symbols = mainMapper.selectAll();
 		String day = lastStatisticsDayMapper.selectByPrimaryKey();
-		List<StockBuySell> list = stockBuySellMapper.selectByDay(day);
-		if(list != null && list.size() > 0){
-			for (StockBuySell stockBuySell : list) {
-				
+		for (String symbol : symbols) {
+			List<StockBuySell> list = stockBuySellMapper.selectByDay(MapUtils.createMap("symbol", symbol, "day", day));
+			if(list != null && list.size() > 0){
+				for (StockBuySell stockBuySell : list) {
+//					stockBuySell.get
+				}
 			}
 		}
 		return null;
